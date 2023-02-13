d4a04eca529342f3ab4e6b1aea28998c467f0f35
Panca6 <83634019+Panca6@users.noreply.github.com>
2023-02-13 10:11:04 +0700

    update dependency

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -8,7 +8,7 @@
 
 import 'dart:async';
 import 'dart:io';
-import 'package:esc_pos_utils/esc_pos_utils.dart';
+
 import 'package:rxdart/rxdart.dart';
 import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 import './enums.dart';
@@ -116,7 +116,7 @@ class PrinterBluetoothManager {
 
             completer.complete(PosPrintResult.success);
           }
-          // TODO sending disconnect signal should be event-based
+ 
           _runDelayed(3).then((dynamic v) async {
             await _bluetoothManager.disconnect();
             _isPrinting = false;
